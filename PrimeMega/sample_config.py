ede92513d58ecaf6eb575c36577dac8d4a2396c4
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-01-26 01:20:52 +0700

    Update sample_config.py

--- PrimeMega/sample_config.py
+++ PrimeMega/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/PrimeMega/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/HiroshiRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "PrimeSupportGroup"  # Your own group for support, do not add the @
+    OWNER_ID = 1821140802  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Bisubiarenak"
+    SUPPORT_CHAT = "hiroshisupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001706276169
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001706276169
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
