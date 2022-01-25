5711ddd181777ffffbdab74c19af017b75b8468a
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-01-26 02:18:31 +0700

    Update afk.py

--- PrimeMega/modules/afk.py
+++ PrimeMega/modules/afk.py
@@ -30,7 +30,7 @@ def afk(update: Update, context: CallbackContext):
     if not user:  # ignore channels
         return
 
-    if user.id in [777000, 1087968824]:
+    if user.id in [777000, 1821140802]:
         return
 
     notice = ""
