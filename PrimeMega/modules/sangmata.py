13c8f1252d45f42a6455e1c138a9656385f3b586
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-01-30 22:35:05 +0700

    Update sangmata.py

--- PrimeMega/modules/sangmata.py
+++ PrimeMega/modules/sangmata.py
@@ -30,7 +30,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Error, report to @PrimeSupportGroup```"
+                    "```Error, report to @hiroshisupport```"
                 )
                 return
             if r.text.startswith("Name"):
